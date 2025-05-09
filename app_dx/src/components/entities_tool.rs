d1ed81e793d59193d1310ff266e94f12a8b5f9fe
doup <asier@illarra.com>
2025-05-09 19:06:33 +0200

    Fix weird scroll jumps when pinning/unpinning entities

--- app_dx/src/components/entities_tool.rs
+++ app_dx/src/components/entities_tool.rs
@@ -10,6 +10,25 @@ pub fn EntitiesTool() -> Element {
     let active_entity = use_context::<EntitiesToolState>().active;
     let pinned_entities = use_context::<EntitiesToolState>().pinned;
 
+    // Merge pinned and active entities so we render the corresponding
+    // inspectors in a single loop. By doing this we avoid weird scroll jumps
+    // when pinning/unpinning entities.
+    let entities = use_memo(move || {
+        let mut entities = pinned_entities()
+            .clone()
+            .into_iter()
+            .map(|id| (id, true))
+            .collect::<Vec<_>>();
+
+        if let Some(id) = active_entity() {
+            if !pinned_entities().contains(&id) {
+                entities.push((id, false));
+            }
+        }
+
+        entities
+    });
+
     rsx! {
         div { class: "sidebar-layout",
             div { class: "sidebar-layout__sidebar",
@@ -17,14 +36,8 @@ pub fn EntitiesTool() -> Element {
             }
 
             div { class: "sidebar-layout__content entities-grid",
-                for id in pinned_entities().iter() {
-                    EntityInspector { id: *id, is_pinned: true }
-                }
-
-                if let Some(id) = active_entity() {
-                    if !pinned_entities().contains(&id) {
-                        EntityInspector { id, is_pinned: false }
-                    }
+                for (id , is_pinned) in entities().iter() {
+                    EntityInspector { key: id, id: *id, is_pinned: *is_pinned }
                 }
             }
         }
