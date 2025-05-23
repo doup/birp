057ee3e781c3160464fa36b65b1bbf42564874e3
doup <asier@illarra.com>
2025-05-23 20:19:58 +0200

    Make components content collapsible

--- app_dx/src/components/component_inspector.rs
+++ app_dx/src/components/component_inspector.rs
@@ -6,6 +6,17 @@ use crate::{components::JsonValue, states::ConnectionState, utils::get_short_typ
 #[component]
 pub fn ComponentInspector(type_path: String, value: Option<Value>) -> Element {
     let schema = use_context::<ConnectionState>().schema;
+    let mut is_open = use_signal(|| true);
+    let header_class = use_memo(move || {
+        format!(
+            "component__header {}",
+            if is_open() {
+                "component__header--open"
+            } else {
+                "component__header--closed"
+            }
+        )
+    });
     let bevy_type = use_memo({
         let type_path = type_path.clone();
         move || schema().get(&type_path.clone()).cloned()
@@ -13,28 +24,34 @@ pub fn ComponentInspector(type_path: String, value: Option<Value>) -> Element {
 
     rsx! {
         div { class: "component",
-            div { class: "component__header", title: "{type_path}",
-                {get_short_type_name(&type_path)}
+            div {
+                class: header_class(),
+                title: "{type_path}",
+                onclick: move |_| is_open.set(!is_open()),
+                span { {get_short_type_name(&type_path)} }
+                {Icon::ChevronDown.render_with_class("component__open-icon")}
             }
 
             div { class: "component__value",
-                {
-                    match bevy_type() {
-                        Some(_bevy_type) => {
-                            if let Some(value) = value {
-                                rsx! {
-                                    // div { style: "font-size: 10px;", "{bevy_type.schema_type:?} {bevy_type.kind:?}" }
-                                    JsonValue { value }
-                                }
-                            } else {
-                                rsx! {
-                                    div { class: "component__no-value", "No value." }
+                if is_open() {
+                    {
+                        match bevy_type() {
+                            Some(_bevy_type) => {
+                                if let Some(value) = value {
+                                    rsx! {
+                                        // div { style: "font-size: 10px;", "{bevy_type.schema_type:?} {bevy_type.kind:?}" }
+                                        JsonValue { value }
+                                    }
+                                } else {
+                                    rsx! {
+                                        div { class: "issue issue--no-value", "No value." }
+                                    }
                                 }
                             }
+                            None => rsx! {
+                                div { class: "issue", "Type not registered." }
+                            },
                         }
-                        None => rsx! {
-                            div { class: "component__not-registered", "Type not registered." }
-                        },
                     }
                 }
             }
